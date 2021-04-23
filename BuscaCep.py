1c509580d19dc9cd020e540a05e69a42b07011da
ErickBarbosas <santoserick2015@hotmail.com>
2021-04-23 19:08:52 -0300

    Criando Projeto

--- BuscaCep.py
+++ BuscaCep.py
@@ -1,1 +1,43 @@
-print ('ola mundo')
+import requests 
+import os 
+
+def main():
+    print('*****************************************')
+    print('*********      Consulta Cep     *********')
+    print('*****************************************')
+    print('\n')
+
+    cep=input('Digite o CEP para consultar: ')
+
+    if len(cep)!=8:
+        print('Cep Invalido')
+        exit()
+        
+    request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
+
+    retorno = request.json()
+
+    os.system('cls') or None
+    if 'erro' not in retorno:
+
+        print('\n\n\CEP Localizado Com Sucesso\n')
+        print('CEP: {}'.format(retorno['cep']))
+        print('Rua: {}'.format(retorno['logradouro']))
+        print('Bairro: {}'.format(retorno['bairro']))
+        print('Cidade: {}'.format(retorno['localidade']))
+        print('UF: {}'.format(retorno['uf']))
+        print('DDD: {}'.format(retorno['ddd']))
+    else:
+        print('\n\nCep Invalido\n\n')
+
+    resposta = int(input('Deseja fazer uma Nova Busca?\n\n1-Sim\n2-Não\n\nR: '))
+
+    if resposta == 1:
+        main()
+    else:
+        exit()
+
+if __name__ == '__main__':
+    main()
+
+
